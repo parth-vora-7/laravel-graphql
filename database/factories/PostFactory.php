a959f05a176e32f5f81140fb9b15b8043a6ab8a8
Parth Vora <parth.vora.777@gmail.com>
2020-11-21 15:22:49 +0530

    Initial commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'author_id' => User::inRandomOrder()->limit(1)->first()->id,
+            'title'     => $this->faker->sentence,
+            'content'   => $this->faker->paragraph,
+        ];
+    }
+}
